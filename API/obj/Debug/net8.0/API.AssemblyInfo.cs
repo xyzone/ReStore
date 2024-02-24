2d321bd7c78df8ba9625d2d905bde25017dbea02
richard <bluetoliu@gmail.com>
2024-02-24 16:42:07 +1100

    tiny

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e108f8dae2e14ca30cba7798c8968c8ea5412b4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c820177700bf1685ae3df4b0d0c59dd107b005a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
